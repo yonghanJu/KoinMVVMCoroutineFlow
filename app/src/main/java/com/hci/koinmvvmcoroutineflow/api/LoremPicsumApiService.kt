37326753f34d15d83a1df891cda44b07fd04b2f2
yonghanJu <ju990828@naver.com>
2022-05-06 20:55:52 +0900

    서비스 함수 추가

--- app/src/main/java/com/hci/koinmvvmcoroutineflow/api/LoremPicsumApiService.kt
+++ app/src/main/java/com/hci/koinmvvmcoroutineflow/api/LoremPicsumApiService.kt
@@ -4,8 +4,15 @@ import okhttp3.ResponseBody
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface LoremPicsumApiService {
     @GET("/id/{imageId}/info")
     suspend fun imageInfo(@Path("imageId") imageId: String): Response<ResponseBody>
+
+    @GET("/v2/list")
+    suspend fun imageList(
+        @Query("page") page: Int?,
+        @Query("limit") limit: Int? = 20
+    ): List<ResImageInfo.Response>
 }
