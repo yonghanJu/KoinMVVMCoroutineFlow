bedf3dd231f2f6479fb32f368eabf77d55a22dbe
yonghanJu <ju990828@naver.com>
2022-05-06 20:57:49 +0900

    Update MainActivity.kt

--- app/src/main/java/com/hci/koinmvvmcoroutineflow/MainActivity.kt
+++ app/src/main/java/com/hci/koinmvvmcoroutineflow/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.hci.koinmvvmcoroutineflow
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.hci.koinmvvmcoroutineflow.ui.loremPicsum.LoremPicsumDetailActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.hci.koinmvvmcoroutineflow.ui.list.LoremPicsumListActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this, LoremPicsumListActivity::class.java))
     }
 }
