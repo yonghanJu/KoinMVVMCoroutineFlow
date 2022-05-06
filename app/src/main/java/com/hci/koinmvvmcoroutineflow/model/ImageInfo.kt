11ac97cd72a420daa2d8cc989f641360c3dfdd2b
yonghanJu <ju990828@naver.com>
2022-05-06 20:09:17 +0900

    사진 모델 수정

--- app/src/main/java/com/hci/koinmvvmcoroutineflow/model/ImageInfo.kt
+++ app/src/main/java/com/hci/koinmvvmcoroutineflow/model/ImageInfo.kt
@@ -3,5 +3,6 @@ package com.hci.koinmvvmcoroutineflow.model
 data class ImageInfo (
     val id: String?,
     val author: String?,
-    val image_url: String?
+    val image_url: String?,
+    val thumbnail_url: String?
 )
