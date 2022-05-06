86da02363fcbfd6cafb5d5e37077f1f2c807c370
yonghanJu <ju990828@naver.com>
2022-05-06 20:09:08 +0900

    Update MainActivity.kt

--- app/src/main/java/com/hci/koinmvvmcoroutineflow/MainActivity.kt
+++ app/src/main/java/com/hci/koinmvvmcoroutineflow/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.hci.koinmvvmcoroutineflow
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hci.koinmvvmcoroutineflow.ui.loremPicsum.LoremPicsumDetailActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
