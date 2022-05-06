f30ecb8bf706405899c2a6034ba2dae5bc83bdeb
yonghanJu <ju990828@naver.com>
2022-05-06 20:10:13 +0900

    뷰모델 생성

---
+++ app/src/main/java/com/hci/koinmvvmcoroutineflow/ui/loremPicsum/LoremPicsumViewModel.kt
@@ -0,0 +1,31 @@
+package com.hci.koinmvvmcoroutineflow.ui.loremPicsum
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hci.koinmvvmcoroutineflow.data.LoremPicsumRepository
+import com.hci.koinmvvmcoroutineflow.model.ImageInfo
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class LoremPicsumViewModel(private val repository: LoremPicsumRepository): ViewModel() {
+
+    private val _imageInfo: MutableLiveData<ImageInfo> = MutableLiveData()
+    val imageInfo: LiveData<ImageInfo> get() = _imageInfo
+
+    private val _errorMsg: MutableLiveData<String> = MutableLiveData()
+    val errorMsg: LiveData<String> get() = _errorMsg
+
+    fun getImageInfo(imageId: String) {
+        viewModelScope.launch {
+            repository.getImageInfo(imageId)
+                .catch {
+                    _errorMsg.postValue(it.message)
+                }.collectLatest {
+                    _imageInfo.postValue(it)
+                }
+        }
+    }
+}
