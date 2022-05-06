587a964670d72826242ddede8af3393d90a25474
yonghanJu <ju990828@naver.com>
2022-05-06 20:08:46 +0900

    레포지토리 생성

---
+++ app/src/main/java/com/hci/koinmvvmcoroutineflow/data/LoremPicsumRepository.kt
@@ -0,0 +1,28 @@
+package com.hci.koinmvvmcoroutineflow.data
+
+import com.google.gson.Gson
+import com.hci.koinmvvmcoroutineflow.api.LoremPicsumApiService
+import com.hci.koinmvvmcoroutineflow.api.ResImageInfo
+import com.hci.koinmvvmcoroutineflow.model.ImageInfo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class LoremPicsumRepository(private val service: LoremPicsumApiService) {
+
+    fun getImageInfo(imageId: String): Flow<ImageInfo> =  flow {
+        val response = service.imageInfo(imageId)
+        if(response.isSuccessful) {
+            kotlin.runCatching {
+                Gson().fromJson(
+                    response.body()?.string(),
+                    ResImageInfo.Response::class.java)
+            }.onSuccess {
+                emit(it.mapper())
+            }.onFailure {
+                throw RuntimeException("invalid JSON File.")
+            }
+        } else {
+            throw RuntimeException("response is Failed.")
+        }
+    }
+}
