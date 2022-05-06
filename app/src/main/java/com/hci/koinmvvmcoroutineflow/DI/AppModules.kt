242c18ccbde9e736a50aef42f4c8f2e8779f45b8
yonghanJu <ju990828@naver.com>
2022-05-06 20:09:02 +0900

    DI 모듈에 레포지토리 추가

--- app/src/main/java/com/hci/koinmvvmcoroutineflow/DI/AppModules.kt
+++ app/src/main/java/com/hci/koinmvvmcoroutineflow/DI/AppModules.kt
@@ -1,11 +1,53 @@
 package com.hci.koinmvvmcoroutineflow.DI
 
+import com.google.gson.GsonBuilder
+import com.hci.koinmvvmcoroutineflow.api.LoremPicsumApiService
+import com.hci.koinmvvmcoroutineflow.data.LoremPicsumRepository
+import com.hci.koinmvvmcoroutineflow.ui.loremPicsum.LoremPicsumViewModel
+import okhttp3.OkHttpClient
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class ApiConstants {
+    companion object {
+        const val API_LOREM_PICSUM = "api.lorem_picsum"
+    }
+}
 
 object AppModules {
-    private val repositories = module {}
-    private val viewModels = module {}
-    private val etc = module {}
+    private val repositories = module {
+        factory { LoremPicsumRepository(service = get(named(ApiConstants.API_LOREM_PICSUM))) }
+    }
+
+    private val viewModels = module {
+        viewModel { LoremPicsumViewModel( repository = get()) }
+    }
+
+    private val etc = module {
+        factory {
+            GsonBuilder()
+                .enableComplexMapKeySerialization()
+                .setPrettyPrinting()
+                .create()
+        }
+        single(named(ApiConstants.API_LOREM_PICSUM)) {
+            Retrofit.Builder()
+                .client(OkHttpClient.Builder().run {
+                    connectTimeout(10, TimeUnit.SECONDS)
+                    readTimeout(10, TimeUnit.SECONDS)
+                    writeTimeout(10, TimeUnit.SECONDS)
+                    build()
+                })
+                .baseUrl("https://picsum.photos")
+                .addConverterFactory(GsonConverterFactory.create(get()))
+                .build()
+                .create(LoremPicsumApiService::class.java)
+        }
+    }
 
     val modules = listOf(etc, repositories, viewModels)
 }
