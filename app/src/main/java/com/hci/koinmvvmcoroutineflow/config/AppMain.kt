273be3525f6084badefc610463c2b644a0e2a34b
yonghanJu <ju990828@naver.com>
2022-05-10 23:03:00 +0900

    [AppMain]에러 로그 추가

--- app/src/main/java/com/hci/koinmvvmcoroutineflow/config/AppMain.kt
+++ app/src/main/java/com/hci/koinmvvmcoroutineflow/config/AppMain.kt
@@ -1,9 +1,11 @@
 package com.hci.koinmvvmcoroutineflow.config
 
 import android.app.Application
-import com.hci.koinmvvmcoroutineflow.DI.AppModules
+import com.hci.koinmvvmcoroutineflow.di.AppModules
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.logger.Level
 
 class AppMain: Application() {
 
@@ -11,6 +13,7 @@ class AppMain: Application() {
         super.onCreate()
 
         startKoin {
+            androidLogger(Level.ERROR)
             androidContext(this@AppMain)
             modules(AppModules.modules)
         }
